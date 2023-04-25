feced5f8edbc64c2192757b4e377aee46d13c053
Freya Connor <Freya.Connor@softwire.com>
2023-04-25 10:21:58 +0100

    passed cherry-pick Red-2 tests and refactored

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -13,18 +13,47 @@ public class Pawn extends AbstractPiece {
         super(Piece.PieceType.PAWN, colour);
     }
 
+    public boolean isFirstMove(Coordinates from) {
+        boolean isFirstMove = true;
+        int row = from.getRow();
+        if(checkColour() == "WHITE" && row != 6) {
+            isFirstMove = false;
+        } else if (checkColour() == "BLACK" && row != 1) {
+            isFirstMove = false;
+        }
+        return isFirstMove;
+    }
+
+    public String checkColour() {
+        String colour = this.colour.name();
+        return colour;
+    }
+
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        //get the colour of the piece instance
-        String colour = this.colour.name();
+        //consider refactoring with isFirstMove method in AbstractPiece
+        int row = from.getRow();
+        System.out.println(row);
         Move validMove;
         //empty list for the possible moves
         ArrayList<Move> possibleMoves = new ArrayList<>();
         //if piece is white, row num -1. If black, +1.
-        if(colour == "WHITE") {
+        if(checkColour() == "WHITE") {
+            //if pawn on row 6 hasnt yet moved, can also move 2 spaces forward
+            if(isFirstMove(from)) {
+                validMove = new Move(from, from.plus(-2, 0));
+                possibleMoves.add(validMove);
+            }
+            // add the single space valid move too
             validMove = new Move(from, from.plus(-1, 0));
             possibleMoves.add(validMove);
-        } else if (colour == "BLACK") {
+        } else if (checkColour() == "BLACK") {
+            //if pawn on row 1 hasnt yet moved, can also move 2 spaces forward
+            if(isFirstMove(from)) {
+                validMove = new Move(from, from.plus(2, 0));
+                possibleMoves.add(validMove);
+            }
+            // add the single space valid move too
             validMove = new Move(from, from.plus(1, 0));
             possibleMoves.add(validMove);
         }
