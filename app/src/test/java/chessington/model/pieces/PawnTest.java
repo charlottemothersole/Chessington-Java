3038165b127b515009882f813ad8dc3ef9fa02bc
Freya Connor <Freya.Connor@softwire.com>
2023-04-25 10:21:58 +0100

    Basic pawn movement tests

--- app/src/test/java/chessington/model/pieces/PawnTest.java
+++ app/src/test/java/chessington/model/pieces/PawnTest.java
@@ -1,7 +1,43 @@
 package chessington.model.pieces;
 
-import static chessington.model.pieces.PieceAssert.*;
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import chessington.model.Board;
+import chessington.model.Coordinates;
+import chessington.model.Move;
+import chessington.model.PlayerColour;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
+    @Test
+    public void whitePawnCanMoveUpOneSquare() {
+        // Arrange
+        Board board = Board.empty();
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(6, 4);
+        board.placePiece(coords, pawn);
+
+        // Act
+        List<Move> moves = pawn.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 0)));
+    }
+
+    @Test
+    public void blackPawnCanMoveDownOneSquare() {
+        // Arrange
+        Board board = Board.empty();
+        Piece pawn = new Pawn(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(1, 4);
+        board.placePiece(coords, pawn);
+
+        // Act
+        List<Move> moves = pawn.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 0)));
+    }
 }
