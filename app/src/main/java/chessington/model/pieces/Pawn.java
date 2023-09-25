a6618f27bb5243ab68838151baf325aaac49a828
Charlotte Mothersole <charlottemothersole@gmail.com>
2023-09-25 14:44:01 +0100

    add colour logic in getAllowedMoves. Both pawn tests passing

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -15,12 +15,19 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        //Use current coords of piece
-        //Calculate valid moves coords - same col, row -1
-        //return the list of the valid moves
-        Move validMove = new Move(from, from.plus(-1, 0));
+        //get the colour of the piece instance
+        String colour = this.colour.name();
+        Move validMove;
+        //empty list for the possible moves
         ArrayList<Move> possibleMoves = new ArrayList<>();
-        possibleMoves.add(validMove);
+        //if piece is white, row num -1. If black, +1.
+        if(colour == "WHITE") {
+            validMove = new Move(from, from.plus(-1, 0));
+            possibleMoves.add(validMove);
+        } else if (colour == "BLACK") {
+            validMove = new Move(from, from.plus(1, 0));
+            possibleMoves.add(validMove);
+        }
         return possibleMoves;
     }
 }
